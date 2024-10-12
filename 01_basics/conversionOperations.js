9d40cb0b828aa08edd00815662250e308d93a1e2
Vedant Johari <65882784+vedantj15@users.noreply.github.com>
2024-10-12 01:04:55 +0000

    datatypes conversion op

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,36 @@
+let score = "Vedant" //33 // 33abc, 
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let s = Number(score)
+
+// console.log(typeof s);
+// console.log(s);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "vedant" => true
+
+let stringNum = 33
+let anostr = String(stringNum)
+
+// console.log(anostr); 33
+// console.log(typeof anostr); // sstring
+let tyrBool = Boolean(stringNum)
+// console.log(typeof tyrBool); // boolean
+// console.log(tyrBool); // true
+
+
+
+
